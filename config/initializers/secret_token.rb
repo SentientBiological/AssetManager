4571ba8a2a70c7cce1ac5b47e3af571e8c88fc25
Russell Johnston <johnston77@gmail.com>
2013-09-17 10:04:18 +1000

    initial merge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssetManager::Application.config.secret_key_base = 'd01650ae1333a5d083e4d6cc1ac16cb476f0429c0c12d7f5f610102cdf48cdb5c7e78a957db875c9fbfaf715d974de1da5b3552f860f1834b0275a987240597b'
